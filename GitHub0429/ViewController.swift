97745a52e8295f3a634798c43ea724d738ed9cef
Peter Kuo <peterkuo00051@gmail.com>
2016-04-29 21:52:08 +0800

    Initial Commit

---
+++ GitHub0429/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub0429
+//
+//  Created by Peter Kuo on 4/29/16.
+//  Copyright © 2016 Peter Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
